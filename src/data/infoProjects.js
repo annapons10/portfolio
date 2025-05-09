4bf9089df36f3647bcc1530dfc9edb952e3ffe11
annapons10 <annapons101435@gmail.com>
2025-05-09 10:02:58 +0200

    fix: corregida la imágen del proyecto portfolio

--- src/data/infoProjects.js
+++ src/data/infoProjects.js
@@ -1,10 +1,11 @@
 import  calculadora  from '../assets/calculadora.png';
 import  juegos  from '../assets/webJuegos.png';
 import seriesTrack from '../assets/seriesTrack.png';
+import portfolio from '../../public/portfolio.png';
 
 export const Projects = [
     {name: 'Calculadora', image:calculadora, description: 'Calculadora web interactiva para realizar operaciones básicas.', url: 'https://annaponsprojects.com/Calculadora/', technologies: ['HTML', 'CSS', 'JavaScript', 'POO', 'Git', 'GitHub']},
     {name: 'Web Juegos', image:juegos, description: 'Colección de juegos prácticando la lógica y la POO.', url: 'https://annaponsprojects.com/webJuegos/', technologies: ['HTML', 'CSS', 'JavaScript', 'POO','Git', 'GitHub']},
-    {name: 'Portfolio', description: 'Mi portfolio personal para mostrar mis proyectos y habilidades.',  url: 'https://annaponsprojects.com/',  technologies: ['HTML', 'TailwindCSS', 'JavaScript', 'React','Git', 'GitHub']}, 
+    {name: 'Portfolio', image:portfolio, description: 'Mi portfolio personal para mostrar mis proyectos y habilidades.',  url: 'https://annaponsprojects.com/',  technologies: ['HTML', 'TailwindCSS', 'JavaScript', 'React','Git', 'GitHub']}, 
     {name: 'Series Track', image:seriesTrack, description: 'App para buscar, ordenar y puntuar localmente series con la API de TMDB.',  url: 'https://annaponsprojects.com/Series/',  technologies: ['HTML', 'Bootstrap', 'JavaScript', 'React','Git', 'GitHub', 'API']}
 ] 
